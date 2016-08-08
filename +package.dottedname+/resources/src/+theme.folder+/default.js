6e62b05291d732ed9fef9b03241a0e1b36829208
Asko Soukka <asko.soukka@iki.fi>
2016-08-08 15:26:33 +0300

    Pin plonetheme-webpack-plugin = 0.2.0

--- +package.dottedname+/resources/src/+theme.folder+/default.js
+++ +package.dottedname+/resources/src/+theme.folder+/default.js
@@ -3,4 +3,7 @@ import 'plone';
 import './default.less';
 import './theme.less';
 
+import jQuery from 'jquery';  // Expose jQuery for convenience
+window.jQuery = jQuery;
+
 window.require = undefined;  // Fix @@search
